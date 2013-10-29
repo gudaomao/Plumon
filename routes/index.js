04df06cc0ae471f60ad415b48d397353db1ff2ba
gudaomao <guodaojob@163.com>
2013-10-29 09:48:48 +0800

    最新版

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,40 @@
 var Note = require('../model/note');
 module.exports = function(app){
     app.get('/',function(req,res){
-        res.render('index',{});
+        var p = req.param('p');
+        var page = parseInt(p);
+        Note.getTen(page,function(err,docs){
+            res.render('index',{
+                docs:docs
+            });
+        });
     });
     app.get('/getten',function(req,res){
-        var page = parseInt(req.query.p);
+        var p=req.param('p');
+        var page = parseInt(p);
         Note.getTen(page,function(err,docs){
+            var d = {msg:err,docs:docs};
+            res.send(d);
+        });
+    });
+    app.get('/getone',function(req,res){
+        Note.getLastOne(function(err,doc){
+            var r ={
+                errmsg:err,
+                doc:doc
+            };
+            res.send(r);
+        });
+    });
+    app.post('/save',function(req,res){
+        var note = new Note(req.param('title'),req.param('codes'));
+        console.log('route-title:'+note.title);
+        console.log('route-content:'+note.codes);
+        note.save(function(err){
             if(err){
-                res.send('出现错误:'+err);
+                res.send(err);
             }
-            res.send(docs);
+            res.send('');
         })
     })
 }
